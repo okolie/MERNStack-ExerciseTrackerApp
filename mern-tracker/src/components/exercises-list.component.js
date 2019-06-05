30e416972f71faa24abf1a98b18d237f500b0fc0
okoliechigozie <okoliechigozie@gmail.com>
2019-06-05 06:39:17 +0100

    Modified create [user, exercise, exercise-list] components

--- mern-tracker/src/components/exercises-list.component.js
+++ mern-tracker/src/components/exercises-list.component.js
@@ -1,7 +1,30 @@
 import React, {Component} from "react";
-
+import {Link} from "react-router-dom"
+import axios from "axios"
 export default class ExercisesList extends Component{
+    constructor(props){
+        super(props);
+        this.deleteExercise = this.deleteExercise.bind(this);
+        this.state = {
+            exercises:[]
+        }
+    }
 
+    componentDidMount(){
+        axios.get("http://localhost:5000/exercises/")
+            .then(res=>{
+                if(res.data.length > 0){
+                    this.setState({exercise:res.data})
+                }
+            }).catch(err=>console.log(err))
+    }
+
+    deleteExercise(id){
+        axios.delete(`http://localhost:5000/exercises/${id}`)
+            .then(res=>{
+                console.log(res.data)
+            });
+    }
     render(){
         return(
             <div>
