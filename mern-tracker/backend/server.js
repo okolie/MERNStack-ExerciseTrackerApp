36079bdfda59a3a1b6c1fa9e302bae925b3df71a
okoliechigozie <okoliechigozie@gmail.com>
2019-08-13 18:28:05 +0100

    Updated Server.js file

--- mern-tracker/backend/server.js
+++ mern-tracker/backend/server.js
@@ -1,37 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const exercisesRouter = require("./routes/exercises.js");
+const usersRouter = require("./routes/users.js");
 
-require("dotenv").config();
+dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
 
 const uri = process.env.ATLAS_URI;
 
-mongoose
-  .connect(uri, { useNewUrlParser: true, useCreateIndex: true })
-  .catch(err => console.error("ERROR: " + err));
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
-
 connection.once("open", () => {
-  console.log("Mongo database connection established sucessfully");
+  console.log("MongoDB database connection established successfully");
 });
 
-const usersRouter = require("./routes/users");
-const exercisesRouter = require("./routes/exercises");
-
-app.use("/users", usersRouter);
 app.use("/exercises", exercisesRouter);
-// app.get("/", (req, res) => {
-//   res.send("God is the greatest everyday");
-// });
+app.use("/users", usersRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server is running on ${port}`));
